ef2e7ee3098fa6bb86d4b48373e3966629333bed
celosama <marcelo.inhaquites@gmail.com>
2016-04-22 20:48:56 -0300

    prints a simple button on screen for testing

--- Minimax/TicTacToe.cs
+++ Minimax/TicTacToe.cs
@@ -1,6 +1,9 @@
 ﻿using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
+using Minimax.Components;
+using Minimax.GameLogic;
+using System.Collections.Generic;
 
 namespace Minimax
 {
@@ -8,22 +11,52 @@ namespace Minimax
     {
         GraphicsDeviceManager graphics;
         SpriteBatch spriteBatch;
+        Texture2D texX, texO, texReset;
+        Button button;
+
+        Dictionary<string, GameObject> gameObjects;
+        Dictionary<string, Texture2D> textures;
 
         public TicTacToe()
         {
             graphics = new GraphicsDeviceManager(this);
+            graphics.PreferredBackBufferWidth = 800;
+            graphics.PreferredBackBufferHeight = 600;
+
             Content.RootDirectory = "Content";
+
+            button = new Button(0, 0, "Button1");
+
+            gameObjects = new Dictionary<string, GameObject>()
+            {
+                { "Button1", button }
+            };
+
+            ComponentLocator.RegisterGameObjects(gameObjects);
         }
 
         protected override void Initialize()
         {
-
             base.Initialize();
         }
 
         protected override void LoadContent()
         {
             spriteBatch = new SpriteBatch(GraphicsDevice);
+
+            texX = Content.Load<Texture2D>("X");
+            texO = Content.Load<Texture2D>("O");
+            texReset = Content.Load<Texture2D>("Reset");
+
+            textures = new Dictionary<string, Texture2D>()
+            {
+                { "X", texX },
+                { "O", texO },
+                { "Reset", texReset },
+                { "Button", texReset }
+            };
+
+            ComponentLocator.RegisterTextures(textures);
         }
 
         protected override void UnloadContent()
@@ -41,6 +74,15 @@ namespace Minimax
         protected override void Draw(GameTime gameTime)
         {
             GraphicsDevice.Clear(Color.CornflowerBlue);
+
+            spriteBatch.Begin();
+
+            foreach (KeyValuePair<string, GameObject> entry in gameObjects)
+            {
+                entry.Value.Draw(spriteBatch, gameTime);
+            }
+
+            spriteBatch.End();
 
             base.Draw(gameTime);
         }
