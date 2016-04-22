dd0278e369b0a04de8019605c6773a9ae2779978
celosama <marcelo.inhaquites@gmail.com>
2016-04-22 19:53:49 -0300

    adds abstract GameObject

---
+++ Minimax/Components/GameObject.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Minimax.Components
+{
+    public abstract class GameObject
+    {
+        public void Update(GameTime gameTime)
+        {
+
+        }
+
+        public void Draw(SpriteBatch spriteBatch, GameTime gameTime)
+        {
+
+        }
+    }
+}
