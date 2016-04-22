de8873d18edb3ccc6c3cc995f807de886c4d20ea
celosama <marcelo.inhaquites@gmail.com>
2016-04-22 18:02:54 -0300

    renames main class and removes useless comments

--- Minimax/Program.cs
+++ Minimax/Program.cs
@@ -14,7 +14,7 @@ namespace Minimax
         [STAThread]
         static void Main()
         {
-            using (var game = new Game1())
+            using (var game = new TicTacToe())
                 game.Run();
         }
     }
