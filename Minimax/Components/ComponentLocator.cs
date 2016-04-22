34a4605847701ceaee7871b74f74e8bab0c63ce8
celosama <marcelo.inhaquites@gmail.com>
2016-04-22 20:40:44 -0300

    adds a component locator

---
+++ Minimax/Components/ComponentLocator.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace Minimax.Components
+{
+    public static class ComponentLocator
+    {
+        static Dictionary<string, GameObject> gameObjects;
+        static Dictionary<string, Texture2D> textures;
+
+        public static void RegisterGameObjects(Dictionary<string, GameObject> gameObjects)
+        {
+            ComponentLocator.gameObjects = gameObjects;
+        }
+
+        public static void RegisterTextures(Dictionary<string, Texture2D> textures)
+        {
+            ComponentLocator.textures = textures;
+        }
+
+        public static Texture2D FindTexture(string identifier)
+        {
+            Texture2D texture;
+
+            textures.TryGetValue(identifier, out texture);
+
+            return texture;
+        }
+
+        public static GameObject FindGameObject(string identifier)
+        {
+            GameObject gameObject;
+
+            gameObjects.TryGetValue(identifier, out gameObject);
+
+            return gameObject;
+        }
+    }
+}
