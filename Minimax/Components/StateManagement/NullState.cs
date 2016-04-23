dbf694b1c3f1dc3f3ce8a685df5b79550b1ba0c8
celosama <marcelo.inhaquites@gmail.com>
2016-04-22 22:02:45 -0300

    adds draw support to NullState

--- Minimax/Components/StateManagement/NullState.cs
+++ Minimax/Components/StateManagement/NullState.cs
@@ -3,11 +3,17 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
 
 namespace Minimax.Components.StateManagement
 {
     public class NullState : State
     {
+        public void Draw(SpriteBatch spriteBatch, GameTime gameTime)
+        {
+            // do nothing
+        }
+
         public void Enter()
         {
             // do nothing
