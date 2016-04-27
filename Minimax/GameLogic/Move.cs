9aff06097eef85288f4e291b6a98805797ee48b5
celosama <marcelo.inhaquites@gmail.com>
2016-04-27 01:30:04 -0300

    adds move representation

---
+++ Minimax/GameLogic/Move.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.Xna.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Minimax.GameLogic
+{
+    public class Move
+    {
+        Point position;
+
+        public Move(Point position)
+        {
+            this.position = position;
+        }
+
+        public Point GetPosition()
+        {
+            return position;
+        }
+    }
+}
