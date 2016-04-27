8eddfd1cd7f5f7b6695983d6721fb3930dc0e322
celosama <marcelo.inhaquites@gmail.com>
2016-04-27 01:29:49 -0300

    adds gamesettings

---
+++ Minimax/GameLogic/GameSettings.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Minimax.GameLogic
+{
+    public class GameSettings
+    {
+        public static char Player1 = 'X';
+        public static char Player2 = 'O';
+        public static char FirstPlayer = Player1;
+        public static string Difficulty = "Normal";
+    }
+}
